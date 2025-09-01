8928033ba62ebf1fcd136cf88b032d01d9f5a73e
Bilal Shaikh <vol1shaikh@gmail.com>
2025-09-02 00:21:23 +0530

    non

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # 2. ADMIN USER ID
 # To find your ID, message @userinfobot on Telegram.
-BOT_ADMIN_ID = { 1463187459, #the wooden
-                7441793409, # non
-                }
+BOT_ADMIN_ID = 7441793409 #the wooden puppet
 
 # 3. ALLOWED CHAT IDs
 ALLOWED_CHAT_IDS = {
